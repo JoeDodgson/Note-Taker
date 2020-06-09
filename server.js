b45774a8ac47f06bf55cb7ba4e9063c0c4646d5a
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-09 16:40:49 +0100

    Created get request handler for api/notes

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ const PORT = 3000;
 // Define paths
 const notesHTMLPath = path.join(__dirname, "public/notes.html");
 const indexHTMLPath = path.join(__dirname, "public/index.html");
-const dbJSONPath = path.join(__dirname, "db/db.json");
 const publicPath = path.join(__dirname, "public");
+const dbPath = path.join(__dirname, "db");
+const dbJSONPath = path.join(dbPath, "/db.json");
 
 // Set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -23,6 +24,7 @@ app.use(express.json());
 
 // Set up the express app to serve the user the content of the public folder
 app.use(express.static(publicPath));
+// app.use(express.static(dbPath));
 
 // Routes
 // notes
@@ -30,21 +32,20 @@ app.get("/notes", function(req, res) {
     res.sendFile(notesHTMLPath);
 });
 
-// index
-app.get("*", function(req, res) {
-    res.sendFile(indexHTMLPath);
-});
-
-
 // Get request for notes data
 app.get("/api/notes", async function(req, res) {
     try {
-    const dbJSONContent = await readFileAsync(dbJSONPath);
-    return res.json(dbJSONContent);
+        const dbJSONContent = await readFileAsync(dbJSONPath);
+        return res.json(JSON.parse(dbJSONContent));
 
     } catch (error) {
         console.log(error);
     }
+});
+
+// index
+app.get("*", function(req, res) {
+    res.sendFile(indexHTMLPath);
 });
 
 // Set up server listener
