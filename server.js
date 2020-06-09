6d26725f0bb491bd1a551b20889b5c640e1297e1
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-09 09:00:15 +0100

    Set up server routes for index.html and notes.html

--- server.js
+++ server.js
@@ -8,12 +8,22 @@ const app = express();
 const PORT = 3000;
 
 // Define paths for notes.html and index.html
-const notesHTMLPath = path.join(__dirname, "public/assets/notes.html");
-const indexHTMLPath = path.join(__dirname, "public/assets/index.html");
+const notesHTMLPath = path.join(__dirname, "public/notes.html");
+const indexHTMLPath = path.join(__dirname, "public/index.html");
 
 // Set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Routes
+app.get("/notes", function(req, res) {
+    res.sendFile(notesHTMLPath);
+});
+
+// Routes
+app.get("*", function(req, res) {
+    res.sendFile(indexHTMLPath);
+});
 
 // Set up server listener
 app.listen(PORT, function() {
